7c2a8c16e43fd9a83483f3858fd6b430d86f0276
ratortor <ratorres2097@gmail.com>
2026-07-08 17:18:13 -0500

    Solucionado el conflicto de fusion en models.py

--- classes/models.py
+++ classes/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 
 
-<<<<<<< HEAD
-      def __str__(self):                  
-          return f"{self.nombre} ({self.grado})"
-=======
 class Curso(models.Model):
     nombre = models.CharField(max_length=100)
     grado = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.nombre} ({self.grado})"
->>>>>>> d3b0cff3351878684c21df801688cc3416a0b663
