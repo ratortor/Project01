049d6494c2a28246e2a278163b6f9d82c16c28fc
ratortor <ratorres2097@gmail.com>
2026-07-07 19:19:47 -0500

    Clean001

--- classes/models.py
+++ classes/models.py
@@ -5,5 +5,5 @@ class Curso(models.Model):
       nombre = models.CharField(max_length=100)                                
       grado = models.CharField(max_length=20)  
 
-      def _str_(self):                  
+      def __str__(self):                  
           return f"{self.nombre} ({self.grado})"
